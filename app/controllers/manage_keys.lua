a01a645da227afb665f025f6a83affd63ab3689e
Pale Jack <palejack@shadowboxx.net>
2023-11-24 19:08:38 -0500

    Adding gitignore as well as some authentication for our user management. Correctly linking to the key management interface from the control panel.

--- app/controllers/manage_keys.lua
+++ app/controllers/manage_keys.lua
@@ -2,14 +2,18 @@ local lapis = require("lapis")
 local UserKeys = require("models").UserKeys
 local app = lapis.Application()
 
--- Endpoint to render the manage keys page
-app:get("/manage_keys", function(self)
+local function require_login(self)
+  if not self.session.current_user then
+    return { redirect_to = "/login" }
+  end
+end
+
+app:get("/manage_keys", require_login, function(self)
   self.user_keys = UserKeys:select()  -- Fetch all keys
   return { render = "manage_keys" }
 end)
 
--- Endpoint to handle key deletion
-app:post("/delete_keys", function(self)
+app:post("/delete_keys", require_login, function(self)
   for _, key_id in ipairs(self.params.key_ids or {}) do
     local key = UserKeys:find(key_id)
     if key then
@@ -19,20 +23,18 @@ app:post("/delete_keys", function(self)
   return { redirect_to = "/manage_keys" }
 end)
 
--- Endpoint to render the add key page
-app:get("/add_key", function(self)
+app:get("/add_key", require_login, function(self)
   return { render = "add_key" }
 end)
 
--- Endpoint to handle key addition
-app:post("/add_key", function(self)
+app:post("/add_key", require_login, function(self)
   local username, pubkey = self.params.username, self.params.pubkey
   if not username or not pubkey then
     return { render = "add_key", error = "Both username and public key are required." }
   end
 
   UserKeys:add_key(username, pubkey)
-  return { redirect_to = "/manage_keys" }  -- Redirect back to the key management page
+  return { redirect_to = "/manage_keys" }
 end)
 
 return app
